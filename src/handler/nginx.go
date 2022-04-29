e338415d8e66d9306f24bce212a75277ef56775c
UnknowName <burtte@sina.com>
2022-04-29 17:09:05 +0800

    parallelism json.Unmarshal

--- src/handler/nginx.go
+++ src/handler/nginx.go
@@ -4,8 +4,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"github.com/opentracing/opentracing-go"
+	"errors"
 	"log"
+	"sync"
+	"sync/atomic"
+
+	"github.com/opentracing/opentracing-go"
+
 	"simple-hpa/src/ingress"
 )
 
@@ -60,20 +65,53 @@ func (ndh *nginxDataHandler) parseDataWithFilter(data []byte, services []string)
 		log.Println("Not NGINX Ingress data, origin string is:", string(data))
 		return nil
 	}
-	jsonByte := byteStrings[1]
-	if !bytes.HasPrefix(jsonByte, []byte("{")) {
-		return nil
-	}
-	accessItem := new(ingress.NGINXAccess)
 	// JSON化之前，去掉URL里面的中文\x
-	if err := json.Unmarshal(bytes.ReplaceAll(jsonByte, []byte("\\x"), []byte("")), accessItem); err != nil {
-		log.Println("To json failed ", err, "origin string:", string(jsonByte))
+	jsonByte := bytes.ReplaceAll(byteStrings[1], []byte("\\x"), []byte(""))
+	accessItem := new(ingress.NGINXAccess)
+	err := Unmarshal(jsonByte, accessItem)
+	if err != nil {
+		log.Println("json failed", err)
 		return nil
 	}
+	// 原始数据有问题，非json化数据
 	for _, service := range services {
 		if service == accessItem.ServiceName() {
 			return accessItem
 		}
+	}
+	return nil
+}
+
+func Unmarshal(data []byte, o ingress.Access) error {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	var cnt uint32
+	go func() {
+		defer wg.Done()
+		err := json.Unmarshal(data, o)
+		if err != nil {
+			atomic.AddUint32(&cnt, 1)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		err := json.Unmarshal(append(data, '}'), o)
+		if err != nil {
+			atomic.AddUint32(&cnt, 1)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		err := json.Unmarshal(data[:len(data)-1], o)
+		if err != nil {
+			atomic.AddUint32(&cnt, 1)
+		}
+	}()
+	wg.Wait()
+	if cnt == 3 {
+		return errors.New(string(data))
 	}
 	return nil
 }
