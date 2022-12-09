d0c0231242d156e1bb9e0a6508d75f32f55635d6
UnknowName <burtte@sina.com>
2022-12-09 17:22:04 +0800

    Optimzation config,support signle service min/max/safe and other

--- src/utils/config.go
+++ src/utils/config.go
@@ -17,14 +17,63 @@ const (
 	defaultIntervalTime = 120
 	defaultIngressType  = "nginx"
 	defaultMinPod       = 1
+	defaultFact         = 1
 )
 
-type autoScaleConfig struct {
-	MaxPod   int32    `yaml:"maxPod"`
-	MinPod   int32    `yaml:"minPod"`
-	MaxQPS   float64  `yaml:"maxQPS"`
-	SafeQPS  float64  `yaml:"safeQPS"`
-	Services []string `yaml:"services"`
+type DefaultConfig struct {
+	AvgTime           int     `yaml:"avgTime"`
+	ScaleIntervalTime int     `yaml:"scaleIntervalTime"`
+	MaxPod            int32   `yaml:"maxPod"`
+	MinPod            int32   `yaml:"minPod"`
+	MaxQps            float64 `yaml:"maxQps"`
+	SafeQps           float64 `yaml:"safeQps"`
+	Factor            int32   `yaml:"factor"`
+}
+
+func newScaleConfig(namespace, svc, minPod, maxPod, safeQps, maxQps, factor string) *scaleServiceConfig {
+	_minPod, err := strconv.Atoi(minPod)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_maxPod, err := strconv.Atoi(maxPod)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_factor, err := strconv.Atoi(factor)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_safeQps, err := strconv.Atoi(safeQps)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_maxQps, err := strconv.Atoi(maxQps)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return &scaleServiceConfig{
+		namespace,
+		svc,
+		int32(_maxPod),
+		int32(_minPod),
+		float64(_maxQps),
+		float64(_safeQps),
+		int32(_factor),
+	}
+}
+
+type scaleServiceConfig struct {
+	Namespace   string  `yaml:"namespace"`
+	ServiceName string  `yaml:"serviceName"`
+	MaxPod      int32   `yaml:"maxPod"`
+	MinPod      int32   `yaml:"minPod"`
+	MaxQps      float64 `yaml:"maxQps"`
+	SafeQps     float64 `yaml:"safeQps"`
+	Factor      int32   `yaml:"factor"`
+}
+
+func (ssc *scaleServiceConfig) String() string {
+	return fmt.Sprintf("{%s.%s, %d, %.2f}", ssc.ServiceName, ssc.Namespace, ssc.MaxPod, ssc.MaxQps)
 }
 
 type listenConfig struct {
@@ -38,12 +87,11 @@ type ForwardConfig struct {
 }
 
 type Config struct {
-	AvgTime           int             `yaml:"avgTime"`
-	ScaleIntervalTime int             `yaml:"scaleIntervalTime"`
-	IngressType       string          `yaml:"ingressType"`
-	Listen            listenConfig    `yaml:"listen"`
-	AutoScale         autoScaleConfig `yaml:"autoScale"`
-	Forwards          []ForwardConfig `yaml:"forwards"`
+	IngressType   string                `yaml:"ingressType"`
+	Default       *DefaultConfig        `yaml:"default"`
+	Listen        listenConfig          `yaml:"listen"`
+	Forwards      []ForwardConfig       `yaml:"forwards"`
+	ScaleServices []*scaleServiceConfig `yaml:"scaleServices"`
 }
 
 func (c *Config) String() string {
@@ -51,31 +99,60 @@ func (c *Config) String() string {
 }
 
 func (c *Config) valid() {
-	if c.AutoScale.MaxQPS < c.AutoScale.SafeQPS {
-		panic("config error, autoScale maxQPS < autoScale.safeQPS")
+	if c.Default.MaxQps < c.Default.SafeQps {
+		panic("config error, default.maxQPS < default.safeQPS")
 	}
-	if c.AutoScale.MaxPod < c.AutoScale.MinPod {
-		panic("config error, autoScale maxPod < autoScale.minPod")
+	if c.Default.MaxPod < c.Default.MinPod {
+		panic("config error, default.maxPod < default.minPod")
 	}
-	if c.AutoScale.MinPod < defaultMinPod {
-		c.AutoScale.MinPod = defaultMinPod
-		log.Println("WARN, config minPod < 1, use ", defaultMinPod)
+	if c.Default.MinPod < defaultMinPod {
+		c.Default.MinPod = defaultMinPod
+		log.Println("WARN, default.minPod < 1, use ", defaultMinPod)
 	}
-	if c.AvgTime <= 0 {
-		c.AvgTime = defaultAvgTime
-		log.Println("INFO config avgTime use default ", defaultAvgTime)
+	if c.Default.AvgTime <= 0 {
+		c.Default.AvgTime = defaultAvgTime
+		log.Println("INFO default.avgTime use default ", defaultAvgTime)
 	}
-	if c.ScaleIntervalTime <= 0 {
-		c.ScaleIntervalTime = defaultIntervalTime
-		log.Println("INFO, config scaleIntervalTime use default ", defaultIntervalTime)
+	if c.Default.ScaleIntervalTime <= 0 {
+		c.Default.ScaleIntervalTime = defaultIntervalTime
+		log.Println("INFO, default.scaleIntervalTime use default ", defaultIntervalTime)
 	}
 	if c.IngressType == "" {
 		c.IngressType = defaultIngressType
 		log.Println("INFO config ingressType use default ", defaultIngressType)
 	}
-	if c.AutoScale.Services == nil {
-		c.AutoScale.Services = make([]string, 0)
-		log.Println("WARN config autoServices not  present,this mean nothing todo")
+	if c.Default.Factor <= 0 {
+		c.Default.Factor = defaultFact
+	}
+	if c.ScaleServices == nil {
+		c.ScaleServices = make([]*scaleServiceConfig, 0)
+		log.Println("WARN config scaleServices not present,this mean nothing to do")
+	}
+	for _, scaleConfig := range c.ScaleServices {
+		if scaleConfig.MinPod <= 0 {
+			scaleConfig.MinPod = c.Default.MinPod
+		}
+		if scaleConfig.MaxPod <= 0 {
+			scaleConfig.MaxPod = c.Default.MaxPod
+		}
+		if scaleConfig.MinPod <= 0 {
+			scaleConfig.MinPod = c.Default.MinPod
+		}
+		if scaleConfig.MaxPod < scaleConfig.MinPod {
+			panic(fmt.Sprintf("%s config err, MaxPod < MinPod", scaleConfig.ServiceName))
+		}
+		if scaleConfig.MaxQps <= 0 {
+			scaleConfig.MaxQps = c.Default.MaxQps
+		}
+		if scaleConfig.SafeQps <= 0 {
+			scaleConfig.SafeQps = c.Default.SafeQps
+		}
+		if scaleConfig.MaxQps < scaleConfig.SafeQps {
+			panic(fmt.Sprintf("%s config err, MaxQps < SafeQps", scaleConfig.ServiceName))
+		}
+		if scaleConfig.Factor <= 0 {
+			scaleConfig.Factor = c.Default.Factor
+		}
 	}
 	if c.Forwards == nil {
 		c.Forwards = make([]ForwardConfig, 0)
@@ -83,39 +160,40 @@ func (c *Config) valid() {
 }
 
 func (c *Config) getEnvConfig() {
-	service := os.Getenv("SCALE_SERVICES")
-	if service != "" {
-		c.AutoScale.Services = strings.Split(service, ",")
-	}
-	safeQps, err := strconv.Atoi(os.Getenv("SAFE_QPS"))
-	if err == nil {
-		c.AutoScale.SafeQPS = float64(safeQps)
-	}
-	maxQps, err := strconv.Atoi(os.Getenv("MAX_QPS"))
-	if err == nil {
-		c.AutoScale.MaxQPS = float64(maxQps)
-	}
-	minPod, err := strconv.Atoi(os.Getenv("MIN_POD"))
-	if err == nil {
-		c.AutoScale.MinPod = int32(minPod)
-	}
-	maxPod, err := strconv.Atoi(os.Getenv("MAX_POD"))
-	if err == nil {
-		c.AutoScale.MaxPod = int32(maxPod)
+	envService := os.Getenv("SCALE_SERVICES")
+	if envService != "" {
+		scaleConfigs := make([]*scaleServiceConfig, 0)
+		log.Println("Fond env SCALE_SERVICES:", envService)
+		for _, _service := range strings.Split(envService, ",") {
+			if _service == "" {
+				continue
+			}
+			items := strings.Split(_service, ":")
+			_serviceNamespace := strings.Split(items[0], ".")
+			if len(items) != 6 || len(_serviceNamespace) != 2 {
+				log.Printf("ERROR env SCALE_SERVICES item %s format error ", _serviceNamespace)
+				log.Fatalln("use service.namespace:minPod:maxPod:safeQps:MaxQps:factor format")
+			}
+			serviceName, namespace := _serviceNamespace[0], _serviceNamespace[1]
+			minPod, maxPod, safeQps, maxQps, factor := items[1], items[2], items[3], items[4], items[5]
+			serviceConfig := newScaleConfig(namespace, serviceName, minPod, maxPod, safeQps, maxQps, factor)
+			scaleConfigs = append(scaleConfigs, serviceConfig)
+		}
+		c.ScaleServices = scaleConfigs
 	}
 	sliceTime, err := strconv.Atoi(os.Getenv("SCALE_INTERVAL_TIME"))
 	if err == nil {
-		c.ScaleIntervalTime = sliceTime
+		c.Default.ScaleIntervalTime = sliceTime
 	}
 	avgTime, err := strconv.Atoi(os.Getenv("AVG_TIME"))
 	if err == nil && avgTime > 0 && avgTime <= maxAvgTime {
-		c.AvgTime = avgTime
+		c.Default.AvgTime = avgTime
 	} else if avgTime != 0 && avgTime > maxAvgTime {
 		// if env AVG_TIME not set, default is 0
-		c.AvgTime = maxAvgTime
+		c.Default.AvgTime = maxAvgTime
 		log.Printf("WARN AVG_TIME env is %d great than %d, reset use %d", avgTime, maxAvgTime, maxAvgTime)
 	} else {
-		log.Printf("WARN AVG_TIME env is %d it's not valid, use config.yaml value %d", avgTime, c.AvgTime)
+		log.Printf("WARN AVG_TIME env is %d it's not valid, use config.yaml value %d", avgTime, c.Default.AvgTime)
 	}
 	ingressType := os.Getenv("INGRESS_TYPE")
 	if ingressType != "" {
--- src/utils/config_test.go
+++ src/utils/config_test.go
@@ -2,16 +2,13 @@ package utils
 
 import (
 	"log"
+	"os"
 	"testing"
 )
 
 func TestNewConfig(t *testing.T) {
+	os.Setenv("SCALE_SERVICES", "daohao.demo-dev:1:2:10:20:1")
 	config := NewConfig("F:\\GoCodes\\simple-hpa\\config.yaml")
-	log.Println(config, config.AutoScale.MaxQPS > 19.8)
-	log.Println(config.AutoScale.Services)
-	for _, svc := range config.AutoScale.Services {
-		log.Println(svc)
-	}
-	log.Printf(config.IngressType)
-	log.Println(config.Forwards)
+	// log.Println(config, config.AutoScale.MaxQPS > 19.8)
+	log.Println(config.ScaleServices, config.Default.MaxPod, config.Default.AvgTime)
 }
